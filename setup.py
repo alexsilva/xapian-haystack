5867104764a6c4ec9c4487dcea9d18ce7bf1e383
alex <geniofuturo@gmail.com>
2020-07-07 12:03:44 -0300

    v2.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='xapian-haystack',
-    version='2.1.1',
+    version='2.1.2',
     description='A Xapian backend for Haystack',
     long_description=read('README.rst'),
     classifiers=[
